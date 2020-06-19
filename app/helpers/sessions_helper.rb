7ea77b4e9269a2f7889d7a4aff344eadfcbaae43
shohei-12 <shoheimaeda10@gmail.com>
2020-06-19 21:45:10 +0900

    Add a session helper method

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -1,6 +1,23 @@
 module SessionsHelper
-  # Log in with a given user
+  # Log in with the given user
   def log_in(user)
     session[:user_id] = user.id
+  end
+
+  # Returns true if the user is logged in
+  def logged_in?
+    !session[:user_id].nil?
+  end
+
+  # Return the logged in user
+  def current_user
+    if user_id = session[:user_id]
+      @current_user = User.find_by(id: user_id)
+    end
+  end
+
+  # Returns true if the given user is logged in
+  def current_user?(user)
+    user && user == current_user
   end
 end
--- spec/helpers/sessions_helper_spec.rb
+++ spec/helpers/sessions_helper_spec.rb
@@ -1,11 +1,59 @@
 require 'rails_helper'
 
 RSpec.describe SessionsHelper, type: :helper do
-  describe '#log_in(user)' do
-    let(:john) { create(:john) }
+  let(:john) { create(:john) }
 
+  describe '#log_in(user)' do
     it 'return true' do
       expect(log_in(john)).to be_truthy
+    end
+  end
+
+  describe '#logged_in?' do
+    context 'when the user is logged in' do
+      before { log_in(john) }
+
+      it 'return true' do
+        expect(logged_in?).to eq true
+      end
+    end
+
+    context 'when the user is not logged in' do
+      it 'return false' do
+        expect(logged_in?).to eq false
+      end
+    end
+  end
+
+  describe '#current_user' do
+    context 'when the user is logged in' do
+      before { log_in(john) }
+
+      it 'return the logged in user' do
+        expect(current_user).to eq john
+      end
+    end
+
+    context 'when the user is not logged in' do
+      it 'return nil' do
+        expect(current_user).to eq nil
+      end
+    end
+  end
+
+  describe '#current_user?(user)' do
+    context 'when the given user is logged in' do
+      before { log_in(john) }
+
+      it 'return true' do
+        expect(current_user?(john)).to eq true
+      end
+    end
+
+    context 'when the given user is not logged in' do
+      it 'return false' do
+        expect(current_user?(john)).to eq false
+      end
     end
   end
 end
