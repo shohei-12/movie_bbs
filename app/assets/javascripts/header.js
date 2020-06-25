0d399777a82ebd70972aab36a1e66b497c5f0c22
shohei-12 <shoheimaeda10@gmail.com>
2020-06-25 23:48:25 +0900

    Fix header

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,28 @@
+$(function () {
+  // Hamburger menu
+
+  $(".menu-btn").click(function () {
+    $(this).toggleClass("active");
+    $(".menu").slideToggle(200);
+  });
+
+  $(window).resize(function () {
+    var x = $(window).width();
+    var y = 768;
+    if (x > y) {
+      $(".menu-btn").removeClass("active");
+      $(".menu").removeAttr("style");
+    }
+  });
+
+  // Show current navigation
+
+  $("header nav a").each(function () {
+    var $href = $(this).attr("href");
+    if (location.href.match($href)) {
+      $(this).addClass("current");
+    } else {
+      $(this).removeClass("current");
+    }
+  });
+});
