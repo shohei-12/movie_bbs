d71caa5418754a999bcd916c48e05cbc995ba1b2
shohei-12 <shoheimaeda10@gmail.com>
2020-06-27 23:38:19 +0900

    Fix user login page design

--- app/assets/javascripts/users.js
+++ app/assets/javascripts/users.js
@@ -1,31 +1,31 @@
 $(function () {
   // Change the color of the icon when the input field is focused
 
-  $(".user-signup #user_name").focus(function () {
-    $(".user-signup .fa-user").css("color", "#000");
+  $(".form-name-field").focus(function () {
+    $(".fa-user").css("color", "#000");
   });
-  $(".user-signup #user_name").blur(function () {
-    $(".user-signup .fa-user").css("color", "#aaa");
-  });
-
-  $(".user-signup #user_email").focus(function () {
-    $(".user-signup .fa-envelope").css("color", "#000");
-  });
-  $(".user-signup #user_email").blur(function () {
-    $(".user-signup .fa-envelope").css("color", "#aaa");
+  $(".form-name-field").blur(function () {
+    $(".fa-user").css("color", "#aaa");
   });
 
-  $(".user-signup #user_password").focus(function () {
-    $(".user-signup .key").css("color", "#000");
+  $(".form-email-field").focus(function () {
+    $(".fa-envelope").css("color", "#000");
   });
-  $(".user-signup #user_password").blur(function () {
-    $(".user-signup .key").css("color", "#aaa");
+  $(".form-email-field").blur(function () {
+    $(".fa-envelope").css("color", "#aaa");
   });
 
-  $(".user-signup #user_password_confirmation").focus(function () {
-    $(".user-signup .c-key").css("color", "#000");
+  $(".form-password-field").focus(function () {
+    $(".key").css("color", "#000");
   });
-  $(".user-signup #user_password_confirmation").blur(function () {
-    $(".user-signup .c-key").css("color", "#aaa");
+  $(".form-password-field").blur(function () {
+    $(".key").css("color", "#aaa");
+  });
+
+  $(".form-confirmation-field").focus(function () {
+    $(".c-key").css("color", "#000");
+  });
+  $(".form-confirmation-field").blur(function () {
+    $(".c-key").css("color", "#aaa");
   });
 });
