fab08c5988ac45954920054175cfd8dd9950127a
shohei-12 <shoheimaeda10@gmail.com>
2020-07-05 22:57:25 +0900

    Add an easy login function

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root 'home#top'
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
+  post '/guest_login', to: 'sessions#guest'
   delete '/logout', to: 'sessions#destroy'
   resources :users
   resources :posts
