c76b499d547dd04822e359a715b9cf4d9678dccc
shohei-12 <shoheimaeda10@gmail.com>
2020-07-14 13:59:15 +0900

    feat: add remember me feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  attr_accessor :remember_token
+
   has_one_attached :image
   has_many :posts, dependent: :destroy
   has_many :relationships, dependent: :destroy
@@ -42,5 +44,39 @@ class User < ApplicationRecord
   # Return a true if follow a given user
   def following?(other_user)
     followings.include?(other_user)
+  end
+
+  # Return a random token
+  def self.new_token
+    SecureRandom.urlsafe_base64
+  end
+
+  # Return a hash value of the passed string
+  def self.digest(string)
+    cost =
+      if ActiveModel::SecurePassword.min_cost
+        BCrypt::Engine::MIN_COST
+      else
+        BCrypt::Engine.cost
+      end
+    BCrypt::Password.create(string, cost: cost)
+  end
+
+  # Store users in a database for use in a persistent session
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
+  end
+
+  # Returns true if the token given matches the digest
+  def authenticated?(remember_token)
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  end
+
+  # Destroy a user's login information
+  def forget
+    update_attribute(:remember_digest, nil)
   end
 end
